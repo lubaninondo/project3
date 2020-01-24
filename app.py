ad0fb8b7757ad82ceca27ffa3b6b04e13ac51ccf
Ubuntu <ubuntu@ip-172-31-44-113.eu-west-1.compute.internal>
2020-01-24 11:42:02 +0000

    modified app

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def login():
     if login_user:
         if (request.form['pass']) == login_user['password']:
             session['username'] = request.form['username']
-            return redirect(url_for('get_tasks'))
+            return redirect(url_for('get_recipes'))
         flash('Invalid username/password combination')
 
     return render_template('login.html')
@@ -45,57 +45,57 @@ def register():
                             'email' : request.form['email']
             })
             session['username'] = request.form['username']
-            return redirect(url_for('get_tasks'))
+            return redirect(url_for('get_recipes'))
         flash("Username already exist! Try again!")
     return render_template('register.html')
 
-@app.route('/add_task')
-def add_task():
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId()})
-    return render_template('addtask.html',task=the_task,)
+@app.route('/add_recipe')
+def add_recipe():
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId()})
+    return render_template('addrecipe.html',recipe=the_recipe,)
 
     
-@app.route('/edit_task/<task_id>')
-def edit_task(task_id):
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     all_categories =  mongo.db.categories.find()
-    return render_template('edittask.html', task=the_task,
+    return render_template('editrecipe.html', recipe=the_recipe,
     categories=all_categories)
 
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())    
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())    
 
 
 
-@app.route('/insert_task', methods=['POST'])
-def insert_task():
-    tasks =  mongo.db.tasks
-    tasks.insert_one(request.form.to_dict())
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
     authors = mongo.db.authors
     authors.insert_one({"author" : request.form.get("author")})
-    return redirect(url_for('get_tasks'))
+    return redirect(url_for('get_recipes'))
 
 
-@app.route('/update_task/<task_id>', methods=["POST"])
-def update_task(task_id):
-    tasks = mongo.db.tasks
-    tasks.update( {'_id': ObjectId(task_id)},
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
     {
         'recipe_name':request.form.get('recipe_name'),
         'ingredients':request.form.get('ingredients'),
-        'task_description': request.form.get('task_description'),
+        'recipe_description': request.form.get('recipe_description'),
         'allergens': request.form.get('allergens'),
         'author': request.form.get('username')
     })
     authors = mongo.db.authors
     authors.insert_one({"author" : request.form.get("author")})
-    return redirect(url_for('get_tasks'))
+    return redirect(url_for('get_recipes'))
 
-@app.route('/delete_task/<task_id>')
-def delete_task(task_id):
-    mongo.db.tasks.remove({'_id': ObjectId(task_id)}, )
-    return redirect(url_for('get_tasks'))
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)}, )
+    return redirect(url_for('get_recipes'))
     
 @app.route('/get_categories')
 def get_categories():
